c67f7927dedb9a010c7e3a2771d1a2a13eb14ce5
Liu Chang <liuchang_9483@qq.com>
2019-04-04 22:07:55 -0700

    Update Naive backtracking.cpp

--- Combinations/Naive backtracking.cpp
+++ Combinations/Naive backtracking.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& ans, vector<int> v, vector<int> n,int k){
-        int len=n.size();
+    void helper(vector<vector<int>>& ans, vector<int> v,int start,int k,int n){
         if(v.size()==k){
             ans.push_back(v);
             return;
         }
-        for(int i=0;i<len;++i){
+        for(int i=start;i<=n;++i){
             //swap(n[0],n[i]);
-            v.push_back(n[i]);
-            helper(ans,v,vector<int>(n.begin()+i+1,n.end()),k);
+            v.push_back(i);
+            helper(ans,v,i+1,k,n);
             v.pop_back();
         }
     }
 
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        vector<int> arr={};
-        for(int i=0;i<n;++i) arr.push_back(i+1);
-        helper(ans,{},arr,k);
+        helper(ans,{},1,k,n);
             return ans;
     }
 };
